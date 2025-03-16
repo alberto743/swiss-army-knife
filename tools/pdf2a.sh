e7212d0ebc272729f3070d82b4f1e28a2e7d7785
Alberto <4104972+alberto743@users.noreply.github.com>
2025-03-16 17:26:30 +0100

    [pdf2a] remove PDF annotations, that are not compatible with PDF/A

--- tools/pdf2a.sh
+++ tools/pdf2a.sh
@@ -4,7 +4,8 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-set -e -u -o pipefail -x
+set -e -u -o pipefail
+#set -x
 
 tmpdir=$(mktemp --directory)
 
@@ -17,20 +18,21 @@ dirout="$(realpath "$(dirname "$1")")"
 
 cp "$filein" "$tmpdir""/doc1.pdf"
 exiftool -all= "$tmpdir""/doc1.pdf"
-qpdf --linearize "$tmpdir""/doc1.pdf" "$tmpdir""/doc2.pdf"
+pdftk "$tmpdir""/doc1.pdf" output - uncompress | sed '/^\/Annots/d' | pdftk - output "$tmpdir""/doc2.pdf" compress
+qpdf --linearize "$tmpdir""/doc2.pdf" "$tmpdir""/doc3.pdf"
 
-gs -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dCompatibilityLevel=1.7 -sOutputFile="$tmpdir""/doc1.pdf" \
+gs -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -dCompatibilityLevel=1.7 -sOutputFile="$tmpdir""/doc4.pdf" \
    -sColorConversionStrategy=RGB -sProcessColorModel=DeviceRGB \
    -dSubsetFonts=true -dEmbedAllFonts=true \
    -c '<</NeverEmbed []>> setdistillerparams' \
-   -f "$tmpdir""/doc2.pdf"
+   -f "$tmpdir""/doc3.pdf"
 
 gs -q -dBATCH -dNOPAUSE -sDEVICE=pdfwrite \
    -dPDFA=2 -sPDFACompatibilityPolicy=2 -dCompatibilityLevel=1.7 \
    -sColorConversionStrategy=RGB -sProcessColorModel=DeviceRGB \
-   -sOutputFile="$tmpdir""/doc3.pdf" \
-   -f "$tmpdir""/doc2.pdf"
+   -sOutputFile="$tmpdir""/doc5.pdf" \
+   -f "$tmpdir""/doc4.pdf"
 
-mv "$tmpdir""/doc3.pdf" "$dirout""/""$fname""_2a.pdf"
-rm "$tmpdir""/doc1.pdf" "$tmpdir""/doc1.pdf_original" "$tmpdir""/doc2.pdf"
+mv "$tmpdir""/doc5.pdf" "$dirout""/""$fname""_2a.pdf"
+rm "$tmpdir""/doc1.pdf" "$tmpdir""/doc1.pdf_original" "$tmpdir""/doc2.pdf" "$tmpdir""/doc3.pdf" "$tmpdir""/doc4.pdf"
 rmdir "$tmpdir"
